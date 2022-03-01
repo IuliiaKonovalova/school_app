a6b4eb60942c3bcac5479246bca7a53eb995c595
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-01 13:34:10 +0800

    Fix bug with templates path

--- school_application/settings.py
+++ school_application/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
           os.path.join(BASE_DIR, 'templates'),
-          os.environ.join(BASE_DIR, 'templates', 'allauth'),
+          os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
