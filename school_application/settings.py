468808ec8d117b87b6b638409dd64de7c1db1cec
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-01 13:53:06 +0800

    Add heroku to allowed hosts

--- school_application/settings.py
+++ school_application/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME'), '127.0.0.1']
 
 
 # Application definition
