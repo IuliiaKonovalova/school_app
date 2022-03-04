fb4fa7e3d08cf12975a605f7a6212c877f0cf525
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-04 20:28:34 +0800

    Add navbar functionality

---
+++ static/js/base.js
@@ -0,0 +1,4 @@
+/*jshint version: 6 */
+const profileMenu = document.getElementById("dropdown-profile");
+const loginSignup = document.getElementById("dropdown-enter");
+const dropdown = document.querySelectorAll(".account__options");
