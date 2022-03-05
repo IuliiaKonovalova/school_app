501e8513ff5a969be835de9c85bc644764af3821
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-05 17:44:20 +0800

    Add allauth base.html stylings

--- school_application/urls.py
+++ school_application/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    # path('profiles/', include('profiles.urls')),
-]
+    path('summernote/', include('django_summernote.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
