4df760730ee03397cbcc989c89a558310eec7998
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-05 19:26:31 +0800

    Add Lesson model to lessons app

--- lessons/models.py
+++ lessons/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from profiles.models import SalesManager, Parent
+from subjects.models import Subject
+from profiles.models import Teacher, Student
 
-
-class Sales(models.Model):
-    """Sales model"""
-    sold_by = models.ForeignKey(SalesManager, on_delete=models.SET_NULL)
-    sold_to = models.ForeignKey(Parent, on_delete=models.SET_NULL)
-    amount = models.IntegerField()
-    date = models.DateTimeField(auto_now_add=True)
+class Lesson(models.Model):
+    """Lesson model"""
+    date = models.DateTimeField()
+    time = models.DateField()
+    subject = models.ForeignKey(Subject, related_name='lessons', on_delete=models.CASCADE)
+    teachers = models.ManyToManyField(Teacher, related_name='lessons')
+    students = models.ManyToManyField(Student, related_name='lessons')
