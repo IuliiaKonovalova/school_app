7525f4a3ca9f12db06d6d1c85ac2ce371ed8cc31
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-06 17:59:43 +0800

    Add change password functionality

--- profiles/views.py
+++ profiles/views.py
@@ -23,3 +23,28 @@ class UserProfileView(View):
             'profiles/user_profile.html',
             {'user_profile': user_profile}
             )
+
+    def post(self, request, phone, *args, **kwargs):
+        user_profile = get_object_or_404(CustomUser, phone=phone)
+        if request.user.is_authenticated:
+            if request.user == user_profile:
+                form = PasswordChangeForm(request.user, request.POST)
+                if form.is_valid():
+                    user = form.save()
+                    update_session_auth_hash(request, user)
+                    return HttpResponseRedirect(
+                        reverse(
+                            'user_profile',
+                            kwargs={'phone': user_profile.phone})
+                            )
+                else:
+                    return render(
+                        request,
+                        'profiles/user_profile.html',
+                        {'user_profile': user_profile, 'password_form': form}
+                        )
+        return render(
+            request,
+            'profiles/user_profile.html',
+            {'user_profile': user_profile}
+            )
