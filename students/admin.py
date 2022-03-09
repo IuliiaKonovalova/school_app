8316f0bfb8f198cfffa3a355382ef53b8b2326c0
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-09 20:11:29 +0800

    Add ModelAdmin to students app

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Student
 
 # Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'parent', 'birthday', 'enrolled', 'classes_left', 'sales_manager', 'notes')
+    search_fields = ('first_name', 'last_name', 'parent', 'sales_manager', 'notes')
