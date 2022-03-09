31759eed3ba69db7b55f82f0055977e849e2474f
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-09 20:37:11 +0800

    Add related names to sales model

--- sales/models.py
+++ sales/models.py
@@ -4,7 +4,7 @@ from profiles.models import SalesManager, Parent
 
 class Sales(models.Model):
     """Sales model"""
-    sold_by = models.ForeignKey(SalesManager, on_delete=models.CASCADE)
-    sold_to = models.ForeignKey(Parent, on_delete=models.CASCADE)
+    sold_by = models.ForeignKey(SalesManager, on_delete=models.CASCADE, related_name='sold')
+    sold_to = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='bought')
     amount = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
