d9f8549e8f3af111d919f65e3a77b5251ed06bb2
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-11 16:30:21 +0800

    Fix imports order in students app view

--- students/views.py
+++ students/views.py
@@ -1,12 +1,9 @@
+"""Views for the students app."""
 from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
 from django.views import View
-
-from profiles.models import CustomUser, SalesManager
 from .models import Student
 from .forms import AddStudentForm
-from django.http import HttpResponseRedirect
-
-
 
 
 class StudentAddView(View):
@@ -14,8 +11,6 @@ class StudentAddView(View):
     def get(self, request):
         """Receive student add form"""
         form = AddStudentForm()
-        # form.fields['sales_manager'].queryset = CustomUser.objects.filter(role=2)
-        # form.fields['parent'].queryset = CustomUser.objects.filter(role=4)
         return render(
             request,
             'students/student_add.html',
@@ -27,12 +22,9 @@ class StudentAddView(View):
         form = AddStudentForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
-            # when saving a student need to exclude relation field from the form
             student = form.save(commit=False)
             sales_manager= form.cleaned_data['sales_manager'][0]
-            # sales_manager = get_object_or_404(CustomUser, pk=sales_manager_id)
             parent = form.cleaned_data['parent'][0]
-            # parent = get_object_or_404(CustomUser, pk=parent_id)
             student.save()
             form.save_m2m()
             student.sales_manager.add(sales_manager)
