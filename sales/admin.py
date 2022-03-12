ccc7140195deb04fe85068f6f7d6498def4f447d
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-12 16:30:15 +0800

    Add admin modal to sales app

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Sales
 
-# Register your models here.
+
+@admin.register(Sales)
+class SalesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sold_by', 'sold_to', 'amount', 'date')
+    search_fields = ('sold_by', 'sold_to', 'amount', 'date')
