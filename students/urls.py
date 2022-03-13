ec7d5dff42c313945006eab20aa6d79ce0d08033
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-13 17:54:43 +0800

    Add student_edit template

--- students/urls.py
+++ students/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('students/', StudentsView.as_view(), name='students'),
     path('students/add/', StudentAddView.as_view(), name='student_add'),
     path('students/<int:pk>/', StudentView.as_view(), name='student_detail'),
+    path('students/<int:pk>/edit/', StudentEditView.as_view(), name='student_edit'),
 
 ]
