97944ea1fd02bfd8a28f62a74f2ac1516568b1ba
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-14 14:26:02 +0800

    Reformat profile urls code to pass validations

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,3 +1,4 @@
+"""Urls for the profiles app."""
 from django.urls import path
 from .views import (
     UserProfileView,
@@ -12,12 +13,44 @@ from .views import (
 
 
 urlpatterns = [
-    path('<username>/', UserProfileView.as_view(), name='user_profile'),
-    path('<username>/search/', SearchMembersView.as_view(), name='search_members'),
-    path('<username>/edit/', UserProfileEditView.as_view(), name='user_profile_edit'),
-    path('<username>/change/', UserProfileEditPasswordView.as_view(), name='user_profile_change_password'),
-    path('<username>/applications/', NewApplicationsView.as_view(), name='new_applications'),
-    path('<username>/applications/<int:pk>/', NewApplicationsDetailView.as_view(), name='application_detail'),
-    path('<username>/applications/<int:pk>/delete/', NewApplicationsDeleteView.as_view(), name='application_delete'),
-    path('<username>/delete/', DeleteMemberView.as_view(), name='delete_member'),
+    path(
+        '<username>/',
+        UserProfileView.as_view(),
+        name='user_profile'
+    ),
+    path(
+        '<username>/search/',
+        SearchMembersView.as_view(),
+        name='search_members'
+    ),
+    path(
+        '<username>/edit/',
+        UserProfileEditView.as_view(),
+        name='user_profile_edit'
+    ),
+    path(
+        '<username>/change/',
+        UserProfileEditPasswordView.as_view(),
+        name='user_profile_change_password'
+    ),
+    path(
+        '<username>/applications/',
+        NewApplicationsView.as_view(),
+        name='new_applications'
+    ),
+    path(
+        '<username>/applications/<int:pk>/',
+        NewApplicationsDetailView.as_view(),
+        name='application_detail'
+    ),
+    path(
+        '<username>/applications/<int:pk>/delete/',
+        NewApplicationsDeleteView.as_view(),
+        name='application_delete'
+    ),
+    path(
+        '<username>/delete/',
+        DeleteMemberView.as_view(),
+        name='delete_member'
+    ),
 ]
