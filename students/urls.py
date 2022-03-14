10ceafd4258672520262c4573758979e31517737
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-14 13:49:47 +0800

    Add urls path for student delition

--- students/urls.py
+++ students/urls.py
@@ -1,9 +1,11 @@
+"""Urls for the students app."""
 from django.urls import path
 from .views import (
     StudentsView,
     StudentAddView,
     StudentView,
     StudentEditView,
+    StudentDeleteView,
     )
 
 
@@ -11,6 +13,14 @@ urlpatterns = [
     path('students/', StudentsView.as_view(), name='students'),
     path('students/add/', StudentAddView.as_view(), name='student_add'),
     path('students/<int:pk>/', StudentView.as_view(), name='student_detail'),
-    path('students/<int:pk>/edit/', StudentEditView.as_view(), name='student_edit'),
-
+    path(
+        'students/<int:pk>/edit/',
+        StudentEditView.as_view(),
+        name='student_edit'
+    ),
+    path(
+        'students/<int:pk>/delete/',
+        StudentDeleteView.as_view(),
+        name='student_delete'
+    ),
 ]
