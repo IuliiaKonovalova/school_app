3a24b2b1cab7eb09f90330743eecad1753168928
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-14 16:30:30 +0800

    Add fix full functionality for editing sales

--- sales/urls.py
+++ sales/urls.py
@@ -1,10 +1,11 @@
 """Urls for the sales app."""
 from django.urls import path
-from .views import SalesView, sales_form
+from .views import SalesView, sales_form, edit_sales
 
 
 urlpatterns = [
     path('sales/', SalesView.as_view(), name='sales_list'),
     path('sales/add/', sales_form, name='sales_form'),
+    path('sales/edit/<int:pk>/', edit_sales, name='edit_sales'),
 
 ]
--- sales/views.py
+++ sales/views.py
@@ -80,21 +80,30 @@ def sales_form(request):
 # Add edit sales for a user with role 0 or 2
 def edit_sales(request, pk):
     """Edit sales"""
-    amount_of_classes = 0
+    # amount_of_classes = 0
     if request.method == 'GET':
-        if request.user.is_authenticated and (request.user.role == 0 or request.user.role == 2):
+        if request.user.is_authenticated and (
+            request.user.role == 0 or request.user.role == 2
+        ):
             sales = Sales.objects.get(id=pk)
             form = SalesForm(instance=sales)
             form.fields['sold_to'].queryset = Parent.objects.all()
             form.fields['student'].queryset = Student.objects.all()
+            student = Student.objects.get(id=sales.student_id)
+            # student should be selected in the form
+            form.fields['student'].initial = student
             amount_of_classes = sales.amount
             return render(
                 request,
-                'sales/sales_form.html',
+                'sales/edit_sales.html',
                 {'form': form}
                 )
     if request.method == "POST":
         sales = Sales.objects.get(id=pk)
+        student = Student.objects.get(id=sales.student_id)
+        amount_of_classes = sales.amount
+        student.classes_left -= amount_of_classes
+        student.save()
         form = SalesForm(request.POST, instance=sales)
         if form.is_valid():
             seller = SalesManager.objects.get(user=request.user)
@@ -102,15 +111,13 @@ def edit_sales(request, pk):
             amount = form.cleaned_data['amount']
             child = form.cleaned_data['student']
             student = Student.objects.get(id=child.id)
-            student.classes_left -= amount_of_classes
             student.classes_left += amount
             student.save()
-            new_sale = Sales.objects.create(
-                sold_by=seller,
-                sold_to=buyer,
-                amount=amount,
-            )
-            new_sale.save()
+            sales.sold_by = seller
+            sales.sold_to = buyer
+            sales.amount = amount
+            sales.student_id = child.id
+            sales.save()
             return HttpResponseRedirect(
                 reverse('sales_list')
             )
