b4d81ce4596ec96311a98de91e4c8ac446268913
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-16 11:31:01 +0800

    Add admin model to lessons app

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Lesson
-# Register your models here.
 
-admin.site.register(Lesson)
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    """Lesson admin."""
+    list_display = ('id', 'date', 'time', 'subject')
+    list_filter = ('teachers', 'students')
+    search_fields = ('date', 'time', 'subject', 'teachers', 'students')
