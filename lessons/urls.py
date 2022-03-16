09c68024aa23773ffb305427f22e012d4903a300
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-16 14:20:47 +0800

    Add urls for deleting lessons

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,10 +1,11 @@
 """Urls for the lessons app."""
 from django.urls import path
-from .views import LessonsView, LessonAddView, LessonEditView
+from .views import LessonsView, LessonAddView, LessonEditView, LessonDeleteView
 
 
 urlpatterns = [
     path('', LessonsView.as_view(), name='lessons_list'),
     path('add/', LessonAddView.as_view(), name='lesson_add'),
     path('<int:pk>/edit/', LessonEditView.as_view(), name='lesson_edit'),
+    path('<int:pk>/delete/', LessonDeleteView.as_view(), name='lesson_delete'),
 ]
