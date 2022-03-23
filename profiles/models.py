4e949d82caa4ebd9de94e658fe1076f76d6333b5
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-03-23 19:02:11 +0800

    Add method to Custom user model to get role choice

--- profiles/models.py
+++ profiles/models.py
@@ -3,18 +3,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# Roles to assign to users
-ROLES = (
+class CustomUser(AbstractUser):
+    """Custom user model."""
+    # Roles to assign to users
+    ROLES = (
         (0, 'boss'),
         (1, 'teacher'),
         (2, 'sales'),
         (3, 'receptionist'),
         (4, 'parent'),
         (5, 'potential user'),
-)
-
-class CustomUser(AbstractUser):
-    """Custom user model"""
+    )
+    
     username = models.CharField(
         max_length=50, blank=False,
         null=True,
@@ -25,6 +25,11 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=30)
     phone = models.CharField(max_length=30)
     role = models.IntegerField(choices=ROLES, default=5)
+
+    # add method to get role choices
+    def get_role_choices(self):
+        """Return role choices."""
+        return dict(self.ROLES)[self.role]
 
     def __str__(self):
         return str(self.username)
