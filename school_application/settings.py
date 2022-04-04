397a66b11c68bb46a639782229624f13b5cc6846
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-04-04 12:46:15 +0800

    Add testcases for sales urls

--- school_application/settings.py
+++ school_application/settings.py
@@ -100,16 +100,16 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'school_application.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
