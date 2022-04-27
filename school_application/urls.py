7af9c9a178003a9d488f01db403dd0aa8e18d0a1
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-04-27 18:28:47 +0800

    Add error pages fundtionality

--- school_application/urls.py
+++ school_application/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,3 +15,6 @@ urlpatterns = [
     path('sales/', include('sales.urls')),
     path('lessons/', include('lessons.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+handler404 = 'school_application.views.handler404'
+handler500 = 'school_application.views.handler500'
