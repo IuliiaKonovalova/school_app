7de330d7740e555e313be7f733ca7aa3333e6d67
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-12-19 13:53:26 +0700

    Change allowed hosts in settings.py

--- school_application/settings.py
+++ school_application/settings.py
@@ -15,7 +15,11 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # Debug
 DEBUG = os.environ['DEBUG'] == 'True'
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME'), '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 INSTALLED_APPS = [
